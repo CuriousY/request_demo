5e0209d3963b15cefe1fa0f39153a7b5e96c68cc
Nik <nickscorpio4u@gmail.com>
2017-08-29 10:07:57 +0000

    landingpage

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var app = express();
 
 app.set("view engine","ejs");
 
+app.get("/",function(req,res){
+   
+   var curiousList = [
+       
+       {name : "Einstien" , image:"https://images.pexels.com/photos/326424/pexels-photo-326424.jpeg?h=350&auto=compress&cs=tinysrgb"},
+       {name : "Newton" , image:"https://images.pexels.com/photos/34676/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb"},
+       {name : "Leonardo" , image:"https://images.pexels.com/photos/326424/pexels-photo-326424.jpeg?h=350&auto=compress&cs=tinysrgb"},
+       {name : "Galleleo" , image:"https://images.pexels.com/photos/34676/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb"},
+       {name : "Colt" , image:"https://images.pexels.com/photos/326424/pexels-photo-326424.jpeg?h=350&auto=compress&cs=tinysrgb"},
+       ];
+       
+       res.render("landing.ejs",{curiousList:curiousList});
+});
+
 app.get("/results",function(req,res){
    
   //("welcome to movie world !!"); //
